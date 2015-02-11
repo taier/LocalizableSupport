f816d88176787194240f9f8b8b3d6aef63b479f6
TheTaier <oneander@gmail.com>
2015-02-11 16:33:35 +0200

    Initial Commit

---
+++ LocalizableSupport/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalizableSupport
+//
+//  Created by Deniss Kaibagarovs on 2/11/15.
+//  Copyright (c) 2015 Deniss Kaibagarovs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
